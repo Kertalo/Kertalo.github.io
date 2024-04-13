4a2e8ce6c99367d92c5326513d6f8940c40bd0c5
Kertalo <koksharov-kir@yandex.ru>
2024-04-13 19:30:19 +0300

    all characters is work

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 "use strict";
 
+const url_characters = "https://rickandmortyapi.com/api/character";
+const url_locations = "https://rickandmortyapi.com/api/location";
+const url_episodes = "https://rickandmortyapi.com/api/episode";
 
+const main = document.querySelector('main')
 
-async function requests() {
-    let responseUrls = axios.get('https://rickandmortyapi.com/api');
-    let urls = (await responseUrls).data;
-
-    let responseCharacters = axios.get(urls.characters);
-    let characters = (await responseCharacters).data;
-    console.log(characters);
+let page = 1;
+async function addContent(str) {
+    let param = "?page=" + page + (str == "" ? "" : "&name=" + str);
+    axios.get(url_characters + param)
+    .then((response) =>
+    {
+        console.log('e');
+        let characters = response.data;
+        if (page == 1)
+            main.innerHTML = "";
+        if (characters.info.next == null)
+            button.style.display = 'none';
+        else
+            button.style.display = 'block';
+        for (let i = 0; i < characters.results.length; i++)
+        {
+            main.insertAdjacentHTML('beforeend',
+            `<button class="content">
+                <div class="content__picture">
+                    <img src="` + characters.results[i].image + `" class="content__picture__img" width="240" height="168" alt="character">
+                </div>
+                <div class="content__text">
+                    <div class="content__text__name">`
+                        + characters.results[i].name +
+                        `</div>
+                    <div class="content__text__species">`
+                        + characters.results[i].species +
+                    `</div>
+                </div>
+            </button>`);
+        }
+    })
+    .catch(() => 
+    {
+        console.log('DOW');
+        main.innerHTML = "";
+        button.style.display = 'none';
+    });
 }
 
-// requests();
+addContent("");
 
-function addButtons() {
-    let main = document.querySelector('main')
-    main.insertAdjacentHTML('beforeend', `
-    <button class="content">
-        <div class="content__picture">
-            <img class="content__picture__img" width="240" height="168" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" height="168" alt="character">
-        </div>
-        <div class="content__text">
-            <div class="content__text__name">
-                Rick Sanchez
-            </div>
-            <div class="content__text__species">
-                Human
-            </div>
-        </div>
-    </button>
-    `);
-}
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    page++;
+    addContent("");
+})
 
-for (let i = 0; i < 10; i++)
-    addButtons();
+const input = document.querySelector('input');
+input.addEventListener('input', function() {
+    page = 1;
+    addContent(input.value);
+})
